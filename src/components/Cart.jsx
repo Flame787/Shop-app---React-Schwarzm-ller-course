da8870ee5358c68d0ab4813f5323fac92250f1cd
Flame787 <mbrezovic77@gmail.com>
2025-01-20 17:23:01 +0100

    destructured context-object

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,17 +5,21 @@ import { useContext } from "react";
 import { CartContext } from "../store/shopping-cart-context";
 
 // export default function Cart({ items, onUpdateItemQuantity }) {  // items-prop not needed anymore
-  export default function Cart({ onUpdateItemQuantity }) {
-  const cartCtx = useContext(CartContext);   // call the useContext-hook and provide the CartContext component as argument
-// cartCtx = is value provided by the CONTEXT (either initial or updated value, depending on state)
+export default function Cart({ onUpdateItemQuantity }) {
+  // const cartCtx = useContext(CartContext);
+  // call the useContext-hook and provide the CartContext component as argument
+  // cartCtx = is value provided by the CONTEXT (either initial or updated value, depending on state)
+  const { items } = useContext(CartContext);
+  // destructured the context-object cartCtx
 
-// const cartCtx = use(CartContext); - alternative of useContext (new, React 19 and >)
-// use-hook is more flexible, it could be used inside of an if-block (while useContext is not so flexible)
-// usually we cannot use React hooks in if- or -for-loops
-// but use-hook is available only in React version 19 or higher
+  // const cartCtx = use(CartContext); - alternative of useContext (new, React 19 and >)
+  // use-hook is more flexible, it could be used inside of an if-block (while useContext is not so flexible)
+  // usually we cannot use React hooks in if- or -for-loops
+  // but use-hook is available only in React version 19 or higher
 
-// const totalPrice = items.reduce(
-  const totalPrice = cartCtx.items.reduce(
+  // const totalPrice = items.reduce(
+  // const totalPrice = cartCtx.items.reduce(
+    const totalPrice = items.reduce(   // this works if we have desctructured the cartCtx-object
     (acc, item) => acc + item.price * item.quantity,
     0
   );
@@ -24,13 +28,18 @@ import { CartContext } from "../store/shopping-cart-context";
   return (
     <div id="cart">
       {/* {items.length === 0 && <p>No items in cart!</p>} */}
-      {cartCtx.items.length === 0 && <p>No items in cart!</p>}
-{/* using 'cartCtx.items' because our CONTEXT-value is an object with property: items: [] */}
+      {/* {cartCtx.items.length === 0 && <p>No items in cart!</p>} */}
+      {/* // using 'cartCtx.items' because our CONTEXT-value is an object with property: items: [] */}
+      {items.length === 0 && <p>No items in cart!</p>}
+      {/* // this works if we have desctructured the cartCtx-object */}
+
       {/* {items.length > 0 && ( */}
-      {cartCtx.items.length > 0 && (
+      {/* {cartCtx.items.length > 0 && ( */}
+      {items.length > 0 && (
         <ul id="cart-items">
           {/* {items.map((item) => { */}
-          {cartCtx.items.map((item) => {
+          {/* {cartCtx.items.map((item) => { */}
+          {items.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
             return (
